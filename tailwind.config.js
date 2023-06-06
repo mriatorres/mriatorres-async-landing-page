ed7addd24f444a130924a64e084f3ec8eebbadb8
Maria <mariaf7458@gmail.com>
2023-06-05 22:51:02 -0500

    Se agrego banner

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: 
+    {
+      fontFamily: {
+        'Darumadrop': ['Darumadrop One', 'cursive'],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
