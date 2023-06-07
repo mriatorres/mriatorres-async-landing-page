8367c47f94abdb7c5f3f753d6c68e5af65f2b321
Maria Fernanda <108082130+mriatorres@users.noreply.github.com>
2023-06-07 07:16:16 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -2,8 +2,7 @@ var url = "https://api.github.com/users/mriatorres/repos?per_page=100";
 
 $.get(url, function(data) {
   var sortedRepos = data.sort((a,b) => parseFloat(b.stargazers_count) - parseFloat(a.stargazers_count));
-  // var repoName = sortedRepos[0].name;
-  // console.log("nombre1", repoName);
+  
   var reponames = [];
   for (let i = 0; i < sortedRepos.length; i++) {
     reponames.push([sortedRepos[i].name]);
@@ -26,9 +25,7 @@ $.get(url, function(data) {
     let divContain = document.getElementById("contain");
 
     for (let i = 0; i < sortedRepos.length; i++) {
-    // divContain.classList.add('bg-red-200');
-    // div.classList.add('space-x-6')
-    // div.classList.add('h-full');
+    
     let title = document.createElement("h1");
     title.classList.add('text-white', 'text-xl','font-extrabold','pb-4');
     title.textContent = reponames[i];
@@ -50,18 +47,12 @@ $.get(url, function(data) {
      divText.classList.add("bg-gray-800", "p-4", "w-80", "h-60", "rounded-md")
     div.appendChild(divContain); 
     
-    // divContain.appendChild(text);
+    
     let linkIcon = document.createElement("i");
   }
-  // var repoDescription = sortedRepos[0].description;
-  // var repoLink = sortedRepos[0].html_url;
+  
   var repoStars = sortedRepos[0].stargazers_count;
   
-  // $(document).ready(function() {
-  //   $("#repoLink").attr('href', repoLink);
-  //   $("#repoTitle").html(names);
-  //   $("#repoStars").html(repoStars);
-  //   $("#repoDescription").html(repoDescription);
-  // })
+  
 })
 
